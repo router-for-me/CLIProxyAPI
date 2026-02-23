409e9e8d36961651d007b0ac33c3492912b12feb
Koosha Paridehpour <kooshapari@gmail.com>
2026-02-23 16:01:56 -0700

    docs: build

---
+++ assets/guides_CHANGELOG_ENTRY_TEMPLATE.md.BCmLEHv7.js
@@ -0,0 +1,17 @@
+import{_ as i,o as a,c as n,ag as e}from"./chunks/framework.DM0yugQT.js";const g=JSON.parse('{"title":"Changelog Entry Template","description":"","frontmatter":{},"headers":[],"relativePath":"guides/CHANGELOG_ENTRY_TEMPLATE.md","filePath":"guides/CHANGELOG_ENTRY_TEMPLATE.md","lastUpdated":1771821654000}'),t={name:"guides/CHANGELOG_ENTRY_TEMPLATE.md"};function l(h,s,p,k,d,o){return a(),n("div",null,[...s[0]||(s[0]=[e(`<h1 id="changelog-entry-template" tabindex="-1">Changelog Entry Template <a class="header-anchor" href="#changelog-entry-template" aria-label="Permalink to &quot;Changelog Entry Template&quot;">​</a></h1><p>Use this under <code>## [Unreleased]</code>:</p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Added</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Changed</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Deprecated</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Removed</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Fixed</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Security</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span></code></pre></div>`,3)])])}const E=i(t,[["render",l]]);export{g as __pageData,E as default};
---
+++ assets/reference_CHANGELOG_ENTRY_TEMPLATE.md.Cj1hkkNl.js
@@ -0,0 +1,17 @@
+import{_ as i,o as a,c as n,ag as e}from"./chunks/framework.DM0yugQT.js";const g=JSON.parse('{"title":"Changelog Entry Template","description":"","frontmatter":{},"headers":[],"relativePath":"reference/CHANGELOG_ENTRY_TEMPLATE.md","filePath":"reference/CHANGELOG_ENTRY_TEMPLATE.md","lastUpdated":1771822020000}'),l={name:"reference/CHANGELOG_ENTRY_TEMPLATE.md"};function t(h,s,p,d,k,r){return a(),n("div",null,[...s[0]||(s[0]=[e(`<h1 id="changelog-entry-template" tabindex="-1">Changelog Entry Template <a class="header-anchor" href="#changelog-entry-template" aria-label="Permalink to &quot;Changelog Entry Template&quot;">​</a></h1><p>Copy this into <code>CHANGELOG.md</code> under <code>## [Unreleased]</code>:</p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Added</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Changed</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Deprecated</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Removed</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Fixed</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">### Security</span></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> ...</span></span></code></pre></div><p>Guidelines:</p><ul><li>Describe behavior change, not implementation internals.</li><li>Keep one bullet per externally visible change.</li></ul>`,5)])])}const c=i(l,[["render",t]]);export{g as __pageData,c as default};
